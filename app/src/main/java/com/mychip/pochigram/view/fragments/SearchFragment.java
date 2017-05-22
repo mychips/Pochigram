0173e979a5cc93fc6d070c1a8d0bc9b3d56b2e0d
mychips <mychip13@hotmail.com>
2017-05-22 12:53:30 -0500

    Reto 1 - Search Fragment

--- app/src/main/java/com/mychip/pochigram/view/fragments/SearchFragment.java
+++ app/src/main/java/com/mychip/pochigram/view/fragments/SearchFragment.java
@@ -3,11 +3,18 @@ package com.mychip.pochigram.view.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mychip.pochigram.R;
+import com.mychip.pochigram.adapter.PictureAdapterRecyclerView;
+import com.mychip.pochigram.model.Picture;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +31,25 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        RecyclerView picturesRecycler = (RecyclerView) view.findViewById(R.id.searchFragment);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2, GridLayoutManager.VERTICAL, false);
+
+        picturesRecycler.setLayoutManager(gridLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView =
+                new PictureAdapterRecyclerView(buildPictures(), R.layout.cardview_picture, getActivity());
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
     }
 
+    public ArrayList<Picture> buildPictures() {
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture("http://www.novalandtours.com/images/guide/guilin.jpg", "Erick Sarmiento", "4 dias", "7 Me Gusta"));
+        pictures.add(new Picture("http://www.enjoyart.com/library/landscapes/tuscanlandscapes/large/Tuscan-Bridge--by-Art-Fronckowiak-.jpg", "Maria Sarmiento", "8 dias", "13 Me Gusta"));
+        pictures.add(new Picture("http://www.educationquizzes.com/library/KS3-Geography/river-1-1.jpg", "Alejandra Juarez", "10 dias", "21 Me Gusta"));
+        return pictures;
+    }
 }
