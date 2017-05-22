834292675859dc8e4f6e221834d761a38b710185
mychips <mychip13@hotmail.com>
2017-05-21 20:54:10 -0500

    Commit inicial

---
+++ app/src/main/java/com/mychip/pochigram/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.mychip.pochigram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.mychip.pochigram.view.ContainerActivity;
+import com.mychip.pochigram.view.CreateAccountActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void goToContainer(View view){
+        Intent intent = new Intent(this, ContainerActivity.class);
+        startActivity(intent);
+    }
+
+    public void goCreateAccount(View view){
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/mychip/pochigram/view/fragments/HomeFragment.java
@@ -0,0 +1,66 @@
+package com.mychip.pochigram.view.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mychip.pochigram.R;
+import com.mychip.pochigram.adapter.PictureAdapterRecyclerView;
+import com.mychip.pochigram.model.Picture;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        showToolbar(getResources().getString(R.string.tab_home), false, view);
+        RecyclerView picturesRecycler = (RecyclerView) view.findViewById(R.id.pictureRecycler);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        picturesRecycler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView =
+                new PictureAdapterRecyclerView(buidPictures(), R.layout.cardview_picture, getActivity());
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
+    }
+
+    public ArrayList<Picture> buidPictures(){
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture("http://www.novalandtours.com/images/guide/guilin.jpg", "Erick Sarmiento", "4 dias", "7 Me Gusta"));
+        pictures.add(new Picture("http://www.enjoyart.com/library/landscapes/tuscanlandscapes/large/Tuscan-Bridge--by-Art-Fronckowiak-.jpg", "Maria Sarmiento", "8 dias", "13 Me Gusta"));
+        pictures.add(new Picture("http://www.educationquizzes.com/library/KS3-Geography/river-1-1.jpg", "Alejandra Juarez", "10 dias", "21 Me Gusta"));
+        return pictures;
+    }
+
+    public void showToolbar(String tittle, boolean upButton, View view){
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(tittle);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
+}
